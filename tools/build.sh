824daf85b5c9b9f910c26c89788a3465dacece04
Yangruipis <yangruipis@163.com>
2021-02-22 23:20:40 +0800

    docs: update blog

--- tools/build.sh
+++ tools/build.sh
@@ -3,4 +3,4 @@
 
 python tools/gen_index.py
 
-find . -name "*.org" | xargs -I {} emacs {} --batch --eval '(setq make-backup-files nil)' --eval "(setq org-html-checkbox-type 'html)" --eval "(setq org-html-htmlize-output-type 'css)" -l $PWD/lib/htmlize.el -f org-html-export-to-html
+find . -name "*.org" | xargs -I {} emacs {} --batch --eval '(setq make-backup-files nil)' --eval "(setq org-html-checkbox-type 'html)" --eval "(setq org-html-htmlize-output-type 'css)" --eval "(setq org-image-actual-width nil)" -l $PWD/lib/htmlize.el -f org-html-export-to-html
